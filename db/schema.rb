0e89c7ade78c6df1fa103cdedb6f8d7ecd9aae71
Thomas Midgett <tmidge20@gmail.com>
2015-02-11 16:02:10 -0500

    adds submission table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211203702) do
+ActiveRecord::Schema.define(version: 20150211210142) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -46,6 +46,11 @@ ActiveRecord::Schema.define(version: 20150211203702) do
 
   create_table "responses", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "submissions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
