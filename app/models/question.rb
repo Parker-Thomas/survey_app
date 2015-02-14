8c92ce2c0e94ca2adf8ecebabce826dcde3e1318
Thomas Midgett <tmidge20@gmail.com>
2015-02-14 15:54:28 -0500

    adds options for editing multiple choice

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,7 @@ class Question < ActiveRecord::Base
   belongs_to :question_type
   has_many :options
   has_many :responses
+  accepts_nested_attributes_for :options,
+      :allow_destroy => true,
+      :reject_if     => :all_blank
 end
--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -1,3 +1,5 @@
 class QuestionType < ActiveRecord::Base
   has_many :questions
+
+  validates_uniqueness_of :name
 end
