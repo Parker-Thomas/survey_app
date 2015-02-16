53fb1813b62dc6263a6237039abc4d7815290498
Tim Hooker <timhooker@me.com>
2015-02-15 21:26:43 -0500

    styling changes

--- app/assets/javascripts/increment.js
+++ app/assets/javascripts/increment.js
@@ -1,14 +1,18 @@
 app.incrementValues = function() {
 
+  // function to give the right values to the forms
+
   var allQuestions = $('.question');
-  var inputs = $('.question input');
+  var inputs = $('.question textarea');
   var selects = $('.question-type');
+
   inputs.each(function(index, input) {
     var inputName = input.name;
     var inputId = input.id;
     var reg = /\d+/;
     input.name = inputName.replace(reg, index);
     input.id = inputId.replace(reg, index);
+    console.log(input);
   });
   selects.each(function(index, select) {
     var inputName = select.name;
@@ -16,7 +20,6 @@ app.incrementValues = function() {
     var reg = /\d+/;
     select.name = inputName.replace(reg, index);
     select.id = inputId.replace(reg, index);
-    console.log(select);
   });
 
 }
